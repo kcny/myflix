abb051544bf13dcd373bfb2c1cf862230965558c
Kudzai Nyandoro <kudzain@gmail.com>
2015-02-03 06:01:26 -0800

    Add and Show Categories

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Myflix::Application.routes.draw do
   get 'ui(/:action)', controller: 'ui'
   get '/home', to: 'videos#index'
 
-  resources :videos
+  resources :videos, except: [:destroy]
+  
+  resources :categories, only: [:show]
 end
