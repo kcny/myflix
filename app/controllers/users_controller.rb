b8f81184d704e9b5ba9499684eab94b60c2794af
Kudzai Nyandoro <kudzain@gmail.com>
2015-02-08 08:51:17 -0800

    Implement pages for; front, register and login.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to log_in_path
+    else
+      render :new
+    end
+  end
+end
+
+def user_params
+  params.require(:user).permit(:email, :password, :full_name)
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Myflix::Application.routes.draw do
-  get 'ui(/:action)', controller: 'ui'
+  root to: 'pages#front'
+  get 'register',     to: 'users#new'
+  get 'log_in',       to: 'sessions#new'
+  get '/home',        to: 'videos#index'
 
-  get '/home', to: 'videos#index'
   resources :videos, except: [:destroy] do 
     collection do
       post :search, to: "videos#search"
     end
   end
-  resources :categories, only: [:show] 
+  get 'ui(/:action)', controller: 'ui'
+  resources :categories, only: [:show]
+  resources :users, only: [:create] 
   
    
 end
