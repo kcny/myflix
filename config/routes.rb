53496b14a4949428f0a4201df996ca099c630c01
Kudzai Nyandoro <kudzain@gmail.com>
2015-02-10 05:11:15 -0800

    Update signup page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Myflix::Application.routes.draw do
   root to: 'pages#front'
   get 'register',       to: 'users#new'
-  get '/sign_in',       to: 'sessions#new'
+  get '/login',       to: 'sessions#new'
   post '/login',        to: 'sessions#create'
   get '/logout',        to: 'sessions#destroy'
   get '/home',          to: 'videos#index'
