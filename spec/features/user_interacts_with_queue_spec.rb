f3403c4fcff840b58091184676c107d662b21762
kudzai <kudzain@gmail.com>
2015-02-24 23:42:45 +0000

    Implement 'A more complicated feature spec'

--- spec/features/user_interacts_with_queue_spec.rb
+++ spec/features/user_interacts_with_queue_spec.rb
@@ -10,32 +10,51 @@ feature "User interacts with queue" do
 
     login
 
-    find("a[href='/videos/#{south_park.id}']").click
-    page.should have_content(south_park.title)
-
-    click_link "+ My Queue"
-    page.should have_content(south_park.title)
-
-    visit video_path(south_park)
-    page.should_not have_content "+ My Queue"
-
-    visit home_path
-    find("a[href='/videos/#{monk.id}']").click
-    click_link "+ My Queue"
-
-    visit home_path
-    find("a[href='/videos/#{family_guy.id}']").click
-    click_link "+ My Queue"
-
-    fill_in "video_#{south_park.id}", with: 3
-    fill_in "video_#{monk.id}", with: 1
-    fill_in "video_#{family_guy.id}", with: 2
-
+    add_video_to_queue(monk)
+    expect_video_to_be_in_queue(monk)
+    
+    visit video_path(monk)
+    expect_link_not_to_be_seen("+ My Queue")
+   
+  
+    add_video_to_queue(south_park)
+    add_video_to_queue(family_guy)
+    
+    set_video_position(monk, 3)
+    set_video_position(south_park, 1)
+    set_video_position(family_guy, 2)
+    update_queue
+    
+    expect_video_position(south_park, 1)
+    expect_video_position(family_guy, 2)
+    expect_video_position(monk, 3)
+  end
+  
+  def update_queue
     click_button "Update Instant Queue"
-
-    expect(find("video_#{monk.id}").value).to eq("1")
-    expect(find("video_#{family_guy.id}").value).to eq("2")
-    expect(find("video_#{south_park.id}").value).to eq("3")
-
+  end
+    
+  def expect_video_to_be_in_queue(video)
+    page.should have_content(video.title)
+  end
+  
+  def expect_link_not_to_be_seen(link_text)
+     page.should_not have_content(link_text)
+    end
+    
+    def add_video_to_queue(video)
+      visit home_path
+      find("a[href='/videos/#{video.id}']").click
+      click_link "+ My Queue"
+    end
+    
+    def set_video_position(video, position)
+      within(:xpath, "//tr[contains(.,'#{video.title}')]") do
+      fill_in "queue_items[][position]", with: position
+      end
+    end
+    
+    def expect_video_position(video, position)
+      expect(find(:xpath, "//tr[contains(.,'#{video.title}')]//input[@type='text']").value).to eq(position.to_s)
+    end
   end 
-end
