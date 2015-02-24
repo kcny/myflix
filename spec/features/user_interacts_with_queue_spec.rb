c6506c352c1d314675be4ecdc5a1bc6f9f18e526
Kudzai Nyandoro <kudzain@gmail.com>
2015-02-24 07:35:04 -0800

    Add 'Update instant Queue' spec.

--- spec/features/user_interacts_with_queue_spec.rb
+++ spec/features/user_interacts_with_queue_spec.rb
@@ -19,5 +19,23 @@ feature "User interacts with queue" do
     visit video_path(south_park)
     page.should_not have_content "+ My Queue"
 
+    visit home_path
+    find("a[href='/videos/#{monk.id}']").click
+    click_link "+ My Queue"
+
+    visit home_path
+    find("a[href='/videos/#{family_guy.id}']").click
+    click_link "+ My Queue"
+
+    fill_in "video_#{south_park.id}", with: 3
+    fill_in "video_#{monk.id}", with: 1
+    fill_in "video_#{family_guy.id}", with: 2
+
+    click_button "Update Instant Queue"
+
+    expect(find("video_#{monk.id}").value).to eq("1")
+    expect(find("video_#{family_guy.id}").value).to eq("2")
+    expect(find("video_#{south_park.id}").value).to eq("3")
+
   end 
 end
