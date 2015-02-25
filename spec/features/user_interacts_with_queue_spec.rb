c50ca8e89c79461fd87780e0337bd88670db2e73
Kudzai Nyandoro <kudzain@gmail.com>
2015-02-25 03:16:01 -0800

    Fix indentation in 'user_interacts_with_queue.rb' file.

--- spec/features/user_interacts_with_queue_spec.rb
+++ spec/features/user_interacts_with_queue_spec.rb
@@ -29,7 +29,7 @@ feature "User interacts with queue" do
     expect_video_position(family_guy, 2)
     expect_video_position(monk, 3)
   end
-  
+
   def update_queue
     click_button "Update Instant Queue"
   end
@@ -41,20 +41,20 @@ feature "User interacts with queue" do
   def expect_link_not_to_be_seen(link_text)
      page.should_not have_content(link_text)
     end
-    
-    def add_video_to_queue(video)
-      visit home_path
-      find("a[href='/videos/#{video.id}']").click
-      click_link "+ My Queue"
+
+  def add_video_to_queue(video)
+    visit home_path
+    find("a[href='/videos/#{video.id}']").click
+    click_link "+ My Queue"
+  end
+
+  def set_video_position(video, position)
+    within(:xpath, "//tr[contains(.,'#{video.title}')]") do
+    fill_in "queue_items[][position]", with: position
     end
-    
-    def set_video_position(video, position)
-      within(:xpath, "//tr[contains(.,'#{video.title}')]") do
-      fill_in "queue_items[][position]", with: position
-      end
-    end
-    
-    def expect_video_position(video, position)
+  end
+
+  def expect_video_position(video, position)
       expect(find(:xpath, "//tr[contains(.,'#{video.title}')]//input[@type='text']").value).to eq(position.to_s)
     end
   end 
