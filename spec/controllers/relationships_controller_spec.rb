0b7f618e6c5b5fa9b3cc63b35fe86179a0b31595
Kudzai Nyandoro <kudzain@gmail.com>
2015-03-15 07:10:23 -0700

    Finish Implementing 'People I follow Page'.

--- spec/controllers/relationships_controller_spec.rb
+++ spec/controllers/relationships_controller_spec.rb
@@ -15,4 +15,39 @@ describe RelationshipsController do
       let(:action) { get :index } 
     end
   end 
+
+  describe "DELETE destroy" do
+      it_behaves_like "requires login" do
+        let(:action) { delete :destroy, id: 2 } 
+      end
+
+    it "redirects to the people page" do
+      anesu = Fabricate(:user)
+      set_current_user(anesu)
+      busi = Fabricate(:user)
+      set_current_user
+      relationship = Fabricate(:relationship, follower: anesu, leader: busi)
+      delete :destroy, id: relationship
+      expect(response).to redirect_to people_path
+    end 
+
+    it "deletes relationship if current user is follower" do 
+      anesu = Fabricate(:user)
+      set_current_user(anesu)
+      busi = Fabricate(:user)
+      relationship = Fabricate(:relationship, follower: anesu, leader: busi)
+      delete :destroy, id: relationship
+      expect(Relationship.count).to eq(0)
+    end
+
+    it "does not delete relationship if current user is not follower" do
+      anesu = Fabricate(:user)
+      set_current_user(anesu)
+      busi = Fabricate(:user)
+      chipo = Fabricate(:user)
+      relationship = Fabricate(:relationship, follower: chipo, leader: busi)
+      delete :destroy, id: relationship
+      expect(Relationship.count).to eq(1)
+    end
+  end
 end
