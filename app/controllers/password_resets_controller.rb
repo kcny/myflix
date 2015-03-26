7ba4744469549d8fbc163f11feaed696e36a8b47
Kudzai Nyandoro <kudzain@gmail.com>
2015-03-26 05:19:18 -0700

    Finish Implementing Password Reset.

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,6 +1,23 @@
 class PasswordResetsController < ApplicationController
   def show
     user = User.where(token: params[:id]).first
-    redirect_to expired_token_path unless user
+    if user
+      @token = user.token
+    else
+      redirect_to expired_token_path 
+    end
+  end
+
+  def create
+    user = User.where(token: params[:token]).first
+    if user
+      user.password = params[:password]
+      user.generate_token
+      user.save
+      flash[:success] = "Your password has been updated."
+      redirect_to login_path
+    else
+      redirect_to expired_token_path
+    end
   end
 end
--- spec/controllers/password_resets_controller_spec.rb
+++ spec/controllers/password_resets_controller_spec.rb
@@ -8,12 +8,52 @@ describe PasswordResetsController do
       get :show, id: '02468'
       expect(response).to render_template :show
     end
+
+    it "sets @token" do 
+      anesu = Fabricate(:user)
+      anesu.update_column(:token, '02468')
+      get :show, id: '02468'
+      expect(assigns(:token)).to eq('02468')
+    end
+
     it "redirects to expired token page if token is invalid" do 
       get :show, id: '02468'
       expect(response).to redirect_to expired_token_path
     end
   end
   
-  describe "POST create" do 
+  describe "POST create" do
+    context "without valid token" do 
+      it "redirects to the login page" do
+        anesu = Fabricate(:user, password: 'old_password')
+        anesu.update_column(:token, '02468')
+        post :create, token: '02468', password: 'new_password'
+        expect(response).to redirect_to login_path
+      end
+      it "updates the user's password" do 
+        anesu = Fabricate(:user, password: 'old_password')
+        anesu.update_column(:token, '02468')
+        post :create, token: '02468', password: 'new_password'
+        expect(anesu.reload.authenticate('new_password')).to be_truthy
+      end
+      it "set the flash success message" do
+        anesu = Fabricate(:user, password: 'old_password')
+        anesu.update_column(:token, '02468')
+        post :create, token: '02468', password: 'new_password'
+        expect(flash[:success]).to be_present 
+      end
+      it "regenerates the user token" do 
+        anesu = Fabricate(:user, password: 'old_password')
+        anesu.update_column(:token, '02468')
+        post :create, token: '02468', password: 'new_password'
+        expect(anesu.reload.token).not_to eq('02468')
+      end
+    end
+    context "with invalid token" do 
+      it "redirects to the expired token path" do
+        post :create, token: '02468', password: 'foobar'
+        expect(response).to redirect_to expired_token_path 
+      end
+    end
   end
 end
