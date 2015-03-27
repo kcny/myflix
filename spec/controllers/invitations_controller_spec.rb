9627e6ce864f7f20a99d513d186e0441dbbb00c2
Kudzai Nyandoro <kudzain@gmail.com>
2015-03-27 07:02:51 -0700

    Fix typo in invitations controller spec file.

--- spec/controllers/invitations_controller_spec.rb
+++ spec/controllers/invitations_controller_spec.rb
@@ -6,7 +6,7 @@ describe InvitationsController do
       set_current_user 
       get :new
       expect(assigns(:invitation)).to be_new_record
-      expect(assigns(:invitations)).to be_instance_of Invitation  
+      expect(assigns(:invitation)).to be_instance_of Invitation  
     end
 
     it_behaves_like "requires login" do 
