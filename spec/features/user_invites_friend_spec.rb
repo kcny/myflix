212a9198201b2c9f6824cab257915e8e05fdd67a
Kudzai Nyandoro <kudzain@gmail.com>
2015-03-31 06:15:18 -0700

    Refactor 'user_invites_friend_spec.rb' feature spec

--- spec/features/user_invites_friend_spec.rb
+++ spec/features/user_invites_friend_spec.rb
@@ -5,32 +5,48 @@ feature 'User invites friend' do
     anesu = Fabricate(:user)
     login(anesu)
 
+    invite_friend
+    friend_accepts_invitation
+    friend_logs_in
+    
+    friend_should_follow(anesu)
+    inviter_should_follow_friend(anesu)
+
+    clear_email
+  end 
+
+  def invite_friend
     visit new_invitation_path
     fill_in "Friend's Name", with: "Jabu Dube"
     fill_in "Friend's Email Address", with: "jabu@example.com"
     fill_in "Message", with: "Hello please join this site."
     click_button "Send Invitation"
-     logout
+    logout
+  end
 
+  def friend_accepts_invitation
     open_email "jabu@example.com"
     current_email.click_link "Accept this invitation"
-
     fill_in "Password", with: "password"
     fill_in "Full Name", with: "Jabu Dube"
     click_button "Sign Up"
+  end
 
+  def friend_logs_in
     fill_in "Email Address", with: "jabu@example.com"
     fill_in "Password", with: "password"
     click_button "Login"
+  end
 
+  def friend_should_follow(user)
     click_link "People"
-    expect(page).to have_content anesu.full_name
+    expect(page).to have_content user.full_name
     logout
+  end
 
-    login(anesu)
+  def inviter_should_follow_friend(inviter)
+    login(inviter)
     click_link "People"
     expect(page).to have_content "Jabu Dube"
-
-    clear_email
-  end 
+  end
 end
