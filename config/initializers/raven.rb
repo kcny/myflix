c888fb2256ad13a00e11a89c0f5e25cf02407161
Kudzai Nyandoro <kudzain@gmail.com>
2015-04-03 04:05:10 -0700

    Add DNS to ENV variable in 'raven.rb'

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -1,6 +1,14 @@
-require 'raven'
+# require 'raven'
 
-Raven.configure do |config|
-  config.dsn = 'https://2dd21f55f45843669fc58604bc0e78d2:bde03fe831af42d3a2ac0c9d0f06b8e1@app.getsentry.com/40870'
+# Raven.configure do |config|
+#   config.dsn = 'https://2dd21f55f45843669fc58604bc0e78d2:bde03fe831af42d3a2ac0c9d0f06b8e1@app.getsentry.com/40870'
+# end
+
+
+if Rails.env.staging? || Rails.env.production?
+  require 'raven'
+
+  Raven.config do |config|
+    config.dns = ENV['SENTRY_DNS']
+  end
 end
-
