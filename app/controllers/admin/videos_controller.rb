a70cf0011531d79d98148ba04f3fbeb9e1aa930a
kudzai <kudzain@gmail.com>
2015-04-07 20:20:59 +0000

    Edit.

--- app/controllers/admin/videos_controller.rb
+++ app/controllers/admin/videos_controller.rb
@@ -17,6 +17,8 @@ class Admin::VideosController < ApplicationController
     end
   end
 
+ private   
+  
  def video_params
   params.require(:video).permit(:title, :small_cover, :large_cover, :description, :category_id, :video_url)
 end
