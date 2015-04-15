b03b1d1eb547b339fe44f5b214acbadacebc2ea1
Kudzai Nyandoro <kudzain@gmail.com>
2015-04-15 07:29:06 -0700

    Add update from PR.

--- app/assets/javascripts/payments.js
+++ app/assets/javascripts/payments.js
@@ -6,17 +6,17 @@ jQuery(function($) {
     Stripe.card.createToken($form, stripeResponseHandler);
     return false;
   });
+
+  function stripeResponseHandler(status, response) {
+    var $form = $('#payment-form');
+
+    if (response.error) {
+      $form.find('.payment-errors').text(response.error.message);
+      $form.find('button').prop('disabled', false);
+    } else {
+      var token = response.id;
+      $form.append($('<input type="hidden" name="stripeToken" />').val(token));
+      $form.get(0).submit();
+    }
+  };
 });
-
-function stripeResponseHandler(status, response) {
-  var $form = $('#payment-form');
-
-  if (response.error) {
-    $form.find('.payment-errors').text(response.error.message);
-    $form.find('button').prop('disabled', false);
-  } else {
-    var token = response.id;
-    $form.append($('<input type="hidden" name="stripeToken" />').val(token));
-    $form.get(0).submit();
-  }
-};
