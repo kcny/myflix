76a9ae2313d8303a024107b91710aac6a0463e27
Kudzai Nyandoro <kudzain@gmail.com>
2015-04-21 06:34:13 -0700

    Request for help with selenium-driver.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,6 +14,7 @@ VCR.configure do |c|
   c.cassette_library_dir = 'spec/cassettes'
   c.hook_into :webmock
   c.configure_rspec_metadata!
+  c.ignore_localhost = true
 end
 
 ActiveRecord::Migration.maintain_test_schema!
@@ -21,9 +22,31 @@ ActiveRecord::Migration.maintain_test_schema!
 RSpec.configure do |config|
   
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
   config.infer_base_class_for_anonymous_controllers = false
   config.order = "random"
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.infer_spec_type_from_file_location!
+    RSpec.configure do |config|
+
+    config.before(:suite) do
+      DatabaseCleaner.clean_with(:truncation)
+    end
+
+    config.before(:each) do
+      DatabaseCleaner.strategy = :transaction
+    end
+
+    config.before(:each, :js => true) do
+      DatabaseCleaner.strategy = :truncation
+    end
+
+    config.before(:each) do
+      DatabaseCleaner.start
+    end
+
+    config.after(:each) do
+      DatabaseCleaner.clean
+    end
+  end
 end
