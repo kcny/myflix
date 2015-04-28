72d8322ecd34521eee8dea2e60679cd03d907da6
kudzai <kudzain@gmail.com>
2015-04-28 04:01:20 +0000

    Create 'Week # 8 branch'.

--- spec/models/stripe_wrapper_spec.rb
+++ spec/models/stripe_wrapper_spec.rb
@@ -1,22 +1,23 @@
 require 'spec_helper'
 
-describe StripeWrapper do
-  describe StripeWrapper::Charge do
-    describe ".create" do
-      it "charges successfully", :vcr do
-        Stripe.api_key = ENV['STRIPE_SECRET_KEY']
-        token = Stripe::Token.create(
-          :card => {
-            :number => "4242424242424242",
-            :exp_month => 4,
-            :exp_year => 2020,
-            :cvc => "314"
-          },
-        ).id
-
+describe StripeWrapper do 
+  let (:valid_token) do
+    Stripe::Token.create(
+      :card => {
+        :number => "4242424242424242",
+        :exp_month => 4,
+        :exp_year => 2020,
+        :cvc => "314"
+        },
+      ).id
+  end
+        
+    describe StripeWrapper::Charge do 
+      describe ".create" do 
+          
         response = StripeWrapper::Charge.create(
           amount: 999,
-          card: token,
+          card: valid_token,
           description: "valid charge"
           )
 
@@ -61,6 +62,20 @@ describe StripeWrapper do
           expect(response.error_message).not_to eq("Your card was declined")
           
       end 
+    end
+  end
+  
+  describe StripeWrapper::Customer do
+    describe ".create" do
+      it "creates a customer wwith valid card" do 
+        anesu = Fabricate(:user)
+        response = StripeWrapper::Customer.create(
+          user: anesu,
+          card: valid_token
+         )
+      end
+      it "creates a customer with valid card"
+      it "does not create a custormer with a declinded card"
     end
   end
 end
