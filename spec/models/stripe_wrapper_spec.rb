84b79a2e4a9854619193017d395148938d94118d
Kudzai Nyandoro <kudzain@gmail.com>
2015-04-28 05:33:18 -0700

    Create subscriptions.

--- spec/models/stripe_wrapper_spec.rb
+++ spec/models/stripe_wrapper_spec.rb
@@ -10,72 +10,75 @@ describe StripeWrapper do
         :cvc => "314"
         },
       ).id
-  end
-        
-    describe StripeWrapper::Charge do 
-      describe ".create" do 
-          
-        response = StripeWrapper::Charge.create(
-          amount: 999,
-          card: valid_token,
-          description: "valid charge"
-          )
-
-        expect(response).to be_successful
-      
-      end
-
-      it "makes a card declined charge", :vcr do
-        token = Stripe::Token.create(
-          :card => {
-            :number => "4000000000000002",
-            :exp_month => 4,
-            :exp_year => 2019,
-            :cvc => "314"
-          }
-        ).id 
-
-        response = StripeWrapper::Charge.create(
-          amount: 999,
-          card: token,
-          description: "an invalid charge"
-          )
-
-          expect(response).not_to be_successful
-       end
-      it "returns the error message for declined charges", :vcr do
-        token = Stripe::Token.create(
-          :card => {
-            :number => "4000000000000002",
-            :exp_month => 4,
-            :exp_year => 2019,
-            :cvc => "314"
-          }
-        ).id
-
-        response = StripeWrapper::Charge.create(
-          amount: 999,
-          card: token,
-          description: "an invalid charge"
-          )
-
-          expect(response.error_message).not_to eq("Your card was declined")
-          
-      end 
+  end    
+  describe StripeWrapper::Charge do 
+    describe ".create" do 
+      response = StripeWrapper::Charge.create(
+        amount: 999,
+        card: valid_token,
+        description: "valid charge"
+        )
+      expect(response).to be_successful
     end
+
+  let(:declined_card_token) do
+    Stripe::Token.create(
+      :card => {
+        :number => "4000000000000002",
+        :exp_month => 4,
+        :exp_year => 2019,
+        :cvc => "314"
+      }
+    ).id  
   end
+
+    it "makes a card declined charge", :vcr do
+      response = StripeWrapper::Charge.create(
+        amount: 999,
+        card: declined_card_token,
+        description: "an invalid charge"
+        )
+        expect(response).not_to be_successful
+     end
+    it "returns the error message for declined charges", :vcr do
+      response = StripeWrapper::Charge.create(
+        amount: 999,
+        card: decline_card_token,
+        description: "an invalid charge"
+        )
+        expect(response.error_message).not_to eq("Your card was declined")  
+    end 
+  end
+end
   
   describe StripeWrapper::Customer do
     describe ".create" do
-      it "creates a customer wwith valid card" do 
+      it "creates a customer wwith a valid card", :vcr do 
         anesu = Fabricate(:user)
         response = StripeWrapper::Customer.create(
           user: anesu,
           card: valid_token
          )
+        expect(response).to be_successfull
       end
-      it "creates a customer with valid card"
-      it "does not create a custormer with a declinded card"
+      it "does not create a custormer with a declinded card", :vcr do
+        anesu = Fabricate(:user)
+        response = StripeWrapper::Customer.create(
+          user: alice,
+          card: declined_card__token
+        )
+        expect(response).to be_successfull 
+      end
+
+      it "returns the error message for declinded card", :vcr do 
+        anesu = Fabricate(:user)
+        response = StripeWrapper::Customer.create(
+          user: alice,
+          card: declined_card_token
+        )
+        expect(response).to eq("Your card has been declined.")
+      end
     end
   end
-end
+
+
